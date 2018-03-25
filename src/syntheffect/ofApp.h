0d463c110c1c9ad52ff935c06ae027521dc4bf48
chao-mu <chao-mu@hackpoetic.com>
2018-03-24 22:03:22 -0400

    Add color displacement shader

--- src/syntheffect/ofApp.h
+++ src/syntheffect/ofApp.h
@@ -6,6 +6,7 @@
 #include "syntheffect/filter/Delay.h"
 #include "syntheffect/filter/Huerific.h"
 #include "syntheffect/filter/SpinZoom.h"
+#include "syntheffect/filter/ColorDisplace.h"
 #include "syntheffect/midi/CmdMicro.h"
 #include "syntheffect/video/Playlist.h"
 
@@ -21,11 +22,14 @@ namespace syntheffect {
 
 		protected:
 			void windowResized(int w, int h);
+			void keyPressed(int c);
 
 		private:
 			shared_ptr<filter::Delay> delay_filter_;
 			shared_ptr<filter::Huerific> hue_filter_;
-			shared_ptr<filter::SpinZoom> spin_zoom_;
+			shared_ptr<filter::SpinZoom> spin_zoom_filter_;
+			shared_ptr<filter::ColorDisplace> color_displace_filter_;
+
 			vector<shared_ptr<filter::FilterBase>> filters_;
 
 			shared_ptr<RtMidiIn> midi_in_;
