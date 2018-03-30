538cfe5a49aa3ccd7eddc84c6e4ec3ca1d32e212
chao-mu <chao-mu@hackpoetic.com>
2018-03-30 17:51:16 -0400

    Remove unused variable

--- src/syntheffect/filter/Delay.cpp
+++ src/syntheffect/filter/Delay.cpp
@@ -8,7 +8,6 @@ namespace syntheffect {
 
         void Delay::setupUniforms() {
             float intensity = getIntensity(0, 1);
-            float weight = intensity;
             shader_.setUniform1f("weight", tanh(intensity * 2.5));
             shader_.setUniformTexture("last", last_texture_, 1);
         }
